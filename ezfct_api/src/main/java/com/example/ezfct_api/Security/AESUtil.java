9f73b200bd2f07187f1ce605120a6104d3fa8d5e
SHZTD <tdrag@elpuig.xeill.net>
2025-03-04 17:26:10 +0100

    selectable y envio de datos con ENUM

---
+++ ezfct_api/src/main/java/com/example/ezfct_api/Security/AESUtil.java
@@ -0,0 +1,27 @@
+package com.example.ezfct_api.Security;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class AESUtil {
+    private static final String ALGORITHM = "AES";
+    private static final String SECRET_KEY = "xx_xxxx_xx_xxx!?";
+
+    public static String encrypt(String value) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+        byte[] encrypted = cipher.doFinal(value.getBytes());
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public static String decrypt(String value) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, keySpec);
+        byte[] decodedValue = Base64.getDecoder().decode(value);
+        byte[] decrypted = cipher.doFinal(decodedValue);
+        return new String(decrypted);
+    }
+}
